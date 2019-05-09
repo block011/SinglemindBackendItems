93ec859702a9a0e40512519e5b5e1608831177e0
root <root@instance-1.us-east1-d.c.aesthetic-style-239819.internal>
2019-05-09 23:07:18 +0000

    Add functions for db

--- Texting/DB_Class.py
+++ Texting/DB_Class.py
@@ -13,9 +13,19 @@ class database:
             database = "SingleMind",
             auth_plugin = 'mysql_native_password'
             )
+        self.__cur = __con.cursor()
 
+    #grabs a user by UserID
+    def Grab_User_Data(self,UserID):
+        print("Grabs users info from "+UserID)
 
+    def Grab_Event_Data(self,EventID):
+        print("Grabs event info from " + EventID)
 
+    def Grab_Most_Recent_Notifications(self):
+        print("Grabs most recent notification info")
+    #initialize DB
     def __init__(self):
-        Establish_Connection()
+        self.Establish_Connection()
 
+    
