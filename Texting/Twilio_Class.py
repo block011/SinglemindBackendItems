7793295f3ab1f2221e87c96fa8b69dc800fb53bd
root <root@instance-1.us-east1-d.c.aesthetic-style-239819.internal>
2019-05-10 02:31:35 +0000

    Schedule function

--- Texting/Twilio_Class.py
+++ Texting/Twilio_Class.py
@@ -5,5 +5,15 @@ from twilio.rest import Client
 
 class Twilio:
 
+    def schedule():
+        result = __DB.Grab_Most_Recent_Notification()
+        if result is None:
+            return;
+        for notify in result:
+            print("test")
+        return; 
+
+
+
     def __init__(self):
         __DB = database()
