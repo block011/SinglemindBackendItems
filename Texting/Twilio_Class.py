5c9c9442cf86569b440a71ac3f88580bceb5b44c
root <root@instance-1.us-east1-d.c.aesthetic-style-239819.internal>
2019-05-10 04:36:41 +0000

    Send text function and fixed Schedule

--- Texting/Twilio_Class.py
+++ Texting/Twilio_Class.py
@@ -5,8 +5,22 @@ from twilio.rest import Client
 
 class Twilio:
 
-    def schedule():
-        result = __DB.Grab_Most_Recent_Notification()
+
+
+
+    def Send_Text(self, EventName, EventDesc, PhoneNumber):
+        account_sid = "ACf4a101bd3abc4f0793033e4b64e19ed9"
+        auth_token = "7796ab060c5c53cadc893ec7aa00cf2e"
+        client = Client(account_sid, auth_token)
+
+        message = client.messages.create(
+                body= "Your event is soon!" + EventName + ":" + EventDesc,
+                from_='+19514827423',
+                to=PhoneNumber
+                )
+
+    def Schedule(self):
+        result = self.__DB.Grab_Most_Recent_Notifications()
         if result is None:
             return;
         for notify in result:
